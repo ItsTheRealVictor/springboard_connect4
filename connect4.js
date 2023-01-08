c782b0d87d4fa16f1544dffc460ca86349817835
Victor Delgado <valexdelgado@gmail.com>
2023-01-08 11:55:11 -0800

    trying to come back to this project after a few months and get a better understanding/take a better a shot at completing it without the solution

--- connect4.js
+++ connect4.js
@@ -5,27 +5,28 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-}
+    board = new Array(HEIGHT).fill(null).map(() => new Array(WIDTH).fill(null))
+  }
+
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // Make the top row of the board (the user will click the boxes in this row to add a piece)
+  let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
@@ -36,9 +37,11 @@ function makeHtmlBoard() {
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  // "HEIGHT" number of tr's (table rows) are created
   for (var y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
+
+    // "WIDTH" number of td's (table data cells) are created
     for (var x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
@@ -51,14 +54,21 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div')
+  piece.setAttribute('class', 'piece')
+  piece.setAttribute('class', 'p1')
+  board[y][x] = piece
 }
 
 /** endGame: announce game end */
--- scratch.js
+++ scratch.js
@@ -1,15 +1,38 @@
-const HEIGHT = 6
-const WIDTH = 6
-function makeBoard(len) {
-    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  
-      let arr = new Array(len || 0), i = len
-  
-      if (arguments.length > 1){
-        let args = Array.prototype.slice.call(arguments, 1)
-        while(i--) arr[(len-1) - i] = makeBoard.apply(this, args)
-      }
-      return arr
-    
+const WIDTH = 7;
+const HEIGHT = 6;
+
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+/** makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
+ */
+
+function makeBoard() {
+  board = new Array(HEIGHT).fill().map(() => new Array(WIDTH).fill())
   }
-  const firstBoard = (makeBoard(HEIGHT, WIDTH))
+      
+makeBoard()
+
+
+
+function findSpotForCol(x) {
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+  return null;
+}
+
+
+function placeInTable(y, x) {
+  const piece = document.createElement('div')
+  piece.setAttribute('class', 'piece p1')
+
+  board[y][x] = piece
+  location = document.getElementById(`${y}-${x}`)
+  location.append(piece)
+}
+
+placeInTable(3, 5)
